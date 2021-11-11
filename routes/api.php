bad2e6a0d198701ce7f3dff4ed48c0a4cf70928a
Raju Rayhan <raju.rayhan@yandex.com>
2021-11-12 00:53:17 +0600

    Defined Routes and Controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\SubscriberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('posts', PostController::class)->only([
+    'store'
+]);
+Route::resource('subscribers', SubscriberController::class)->only([
+    'store'
+]);
